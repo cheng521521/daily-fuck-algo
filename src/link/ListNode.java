eed19e57baaf758b41ffc097f74e217075190e32
cheng521521 <15503679582@163.com>
2022-06-22 00:19:17 +0800

    合并两个升序链表

---
+++ src/link/ListNode.java
@@ -0,0 +1,27 @@
+package link;
+
+/**
+ * @author cpx
+ * @Description 链表定义
+ * @date 2022/6/21
+ */
+public class ListNode {
+    private ListNode next;
+    private int val;
+
+    public ListNode getNext() {
+        return next;
+    }
+
+    public void setNext(ListNode next) {
+        this.next = next;
+    }
+
+    public int getVal() {
+        return val;
+    }
+
+    public void setVal(int val) {
+        this.val = val;
+    }
+}
---
+++ src/link/MergeTwoLists.java
@@ -0,0 +1,63 @@
+package link;
+
+/**
+ * @author cpx
+ * @Description 合并两个升序链表
+ * @date 2022/6/22
+ */
+public class MergeTwoLists {
+
+    public static ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+        //设置一个虚拟节点
+        ListNode dummy  = new ListNode();
+        dummy .setVal(-1);
+        ListNode p = dummy;
+        while (l1 != null && l2 != null) {
+            if (l1.getVal() > l2.getVal()) {
+                p.setNext(l2);
+                l2 = l2.getNext();
+            } else {
+                p.setNext(l1);
+                l1 = l1.getNext();
+            }
+            p = p.getNext();
+        }
+        //这里犯错了。
+        //我是用的l1.getnext();在while循环走完以后。每条链都到了最后一个节点。所以不应该在getnext了。
+        if (l1 != null) {
+            p.setNext(l1);
+        }
+        if (l2 != null) {
+            p.setNext(l2);
+        }
+        return dummy.getNext();
+    }
+
+
+
+    public static void main(String[] args) {
+        ListNode l = new ListNode();
+        l.setVal(1);
+        ListNode l1 = new ListNode();
+        l1.setVal(3);
+        ListNode l2 = new ListNode();
+        l2.setVal(5);
+        l.setNext(l1);
+        l1.setNext(l2);
+
+        ListNode p = new ListNode();
+        p.setVal(2);
+        ListNode p1 = new ListNode();
+        p1.setVal(4);
+        ListNode p2 = new ListNode();
+        p2.setVal(7);
+        p.setNext(p1);
+        p1.setNext(p2);
+
+        ListNode listNode = mergeTwoLists(l, p);
+        while (listNode.getNext() != null) {
+            System.out.println(listNode.getVal());
+            listNode = listNode.getNext();
+        }
+    }
+}
