c80ca1e5e71ff8958b61866b6261aed20d366b52
chengpengxiang <pengxiang.cheng@thundercomm.com>
2022-08-11 18:41:07 +0800

    差分数组

---
+++ src/arr/Difference.java
@@ -0,0 +1,25 @@
+package arr;
+
+/**
+ * @ClassName Difference
+ * @Author jackchen
+ * @Date 2022/8/11 12:27
+ * @Description 差分数组
+ **/
+public class Difference {
+    //查分数组
+    private int[] diff;
+
+    //生成一个差分数组
+    public Difference(int[] nums) {
+        this.diff = nums;
+    }
+
+    //操作差分数组
+    public void increment() {}
+
+    //返回结果
+    public int[] result() {
+        //根据差分数组构造结果数组
+    }
+}
