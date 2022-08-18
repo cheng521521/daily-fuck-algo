3c1d24c4060359e22b128139935c9803ab46df2a
cheng521521 <15503679582@163.com>
2022-08-18 23:20:08 +0800

    填充节点的右侧指针Leetcode116

---
+++ src/tree/ConnectNode.java
@@ -0,0 +1,52 @@
+package tree;
+
+/**
+ * @author cpx
+ * @Description
+ * @date 2022/8/18
+ */
+public class ConnectNode {
+    // 主函数
+    Node connect(Node root) {
+        if (root == null) return null;
+        // 遍历「三叉树」，连接相邻节点
+        traverse(root.left, root.right);
+        return root;
+    }
+
+    // 三叉树遍历框架
+    void traverse(Node node1, Node node2) {
+        if (node1 == null || node2 == null) {
+            return;
+        }
+        /**** 前序位置 ****/
+        // 将传入的两个节点穿起来,每个叉要做的事情就是要把两个内部节点连接起来
+        node1.next = node2;
+
+        // 连接相同父节点的两个子节点
+        traverse(node1.left, node1.right);
+        traverse(node2.left, node2.right);
+        // 连接跨越父节点的两个子节点
+        traverse(node1.right, node2.left);
+    }
+
+    class Node {
+        public int val;
+        public Node left;
+        public Node right;
+        public Node next;
+
+        public Node() {}
+
+        public Node(int _val) {
+            val = _val;
+        }
+
+        public Node(int _val, Node _left, Node _right, Node _next) {
+            val = _val;
+            left = _left;
+            right = _right;
+            next = _next;
+        }
+    };
+}
